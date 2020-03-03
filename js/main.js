ba1389e01f241dd557c3c2e60997e95d339e2d24
femmienine <53113092+femmienine@users.noreply.github.com>
2020-03-03 19:49:44 +0100

    added content

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const searchString = document.getElementById("search");
+const a = document.getElementsByClassName("image");
+
+searchString.addEventListener("keyup", () => {
+  const input = searchString.value.toUpperCase();
+
+  for (i = 0; i < a.length; i += 1) {
+    const attrData = a[i].getAttribute("data-title");
+    if (attrData.toUpperCase().indexOf(input) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+});
+
+searchString.addEventListener("search", () => {
+  if (event.target.value === "") {
+    for (i = 0; i < a.length; i += 1) {
+      a[i].style.display = "";
+    }
+  }
+});
